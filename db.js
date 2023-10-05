75ed7c4c9dc210f835f86cf3b7342dbbc5dd88c5
gbaer <gbaer@EYI-PC>
2023-10-04 22:35:40 -0400

    update to db.js

--- db.js
+++ db.js
@@ -1,10 +1,23 @@
 /** Database connection for Microblog. */
 
+// const { Client } = require("pg");
+
+// const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+
+// client.connect();
+
+// module.exports = client;
+
 const { Client } = require("pg");
 
-const client = new Client(process.env.DATABASE_URL || "postgresql:///microblog");
+const client = new Client({
+  user: "baer",
+  host: "localhost",
+  port: 5432,
+  password: "baer",
+  database: "microblog",
+});
 
 client.connect();
-
 
 module.exports = client;
